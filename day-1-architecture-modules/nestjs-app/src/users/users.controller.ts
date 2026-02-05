adbcd3481f2d44ba92241d39a4020915d3d6b52b
Maina.dev <138670482+Mainaaa1@users.noreply.github.com>
2026-02-05 14:47:25 +0300

    Update users.controller.ts

--- day-1-architecture-modules/nestjs-app/src/users/users.controller.ts
+++ day-1-architecture-modules/nestjs-app/src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('api/users')
 export class UsersController {
@@ -13,5 +23,23 @@ export class UsersController {
   @Get(':id')
   getUser(@Param('id') id: string) {
     return this.usersService.findOne(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return this.usersService.update(Number(id), dto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(Number(id));
   }
 }
